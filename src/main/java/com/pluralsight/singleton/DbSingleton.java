5784eab97656112d503a0abcff25cf03b1b9544b
mls <megha_l.s@intuit.com>
2018-08-29 10:35:31 +0530

    Adding thread safe singleton

--- src/main/java/com/pluralsight/singleton/DbSingleton.java
+++ src/main/java/com/pluralsight/singleton/DbSingleton.java
@@ -1,14 +1,23 @@
 package com.pluralsight.singleton;
 
 public class DbSingleton  {
-    private static DbSingleton instance = null;
+    private static volatile DbSingleton instance = null;
     private DbSingleton() {
+        if (instance != null) {
+            //Reflection class cant use constructor
+            throw new RuntimeException("Use getInstance() method to create");
+        }
     }
 
     static DbSingleton getInstance() {
         if (instance == null) {
-            //Lazy loaded but not thread safe  - Creating the object only when you need to use it and not when the JVM loads
-            instance = new DbSingleton();
+            //Rather than synchronize on the whole method, which is performant
+            synchronized (DbSingleton.class) {
+                if (instance == null) {
+                    //Lazy loaded but not thread safe  - Creating the object only when you need to use it and not when the JVM loads
+                    instance = new DbSingleton();
+                }
+            }
         }
         return instance;
     }
